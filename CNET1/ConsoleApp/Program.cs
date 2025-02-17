a76bba9d83de986b01855b1ba5ef72a179b6c7e1
kubicek <82667968+kubicek-skoleni@users.noreply.github.com>
2025-02-17 15:41:52 +0100

    touple metoda bday

--- CNET1/ConsoleApp/Program.cs
+++ CNET1/ConsoleApp/Program.cs
@@ -1,5 +1,19 @@
 ﻿
+// tuple
+(double sum, int count, bool ordered) t1 = (4.5, 3, false);
 
+double x = t1.sum;
 
+double y = x + 5.3;
 
+(int age, bool hadBday) Age(DateTime birth)
+{
+    DateTime now = DateTime.Now;
 
+    TimeSpan diff = now - birth;
+
+    int age = (int)(diff.TotalDays / 365);
+    bool hadBday = now.DayOfYear >= birth.DayOfYear;
+
+    return (age, hadBday);
+}

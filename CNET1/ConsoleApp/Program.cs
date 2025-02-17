db9684c410771646da6b08d09b397d8f1aa6c442
kubicek <82667968+kubicek-skoleni@users.noreply.github.com>
2025-02-17 13:18:22 +0100

    kolekce a foreach

--- CNET1/ConsoleApp/Program.cs
+++ CNET1/ConsoleApp/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿List<int> numbers = [2, 10, -555, 0, 12_000_000, -555];
+
+List<int> numbers2 = new();
+
+numbers2.Add(50);
+var third = numbers.ElementAt(2);
+
+Console.WriteLine($"treti element: {third}");
+
+foreach(int item in numbers)
+{
+    Console.WriteLine(item);
+}
+
+
+
+

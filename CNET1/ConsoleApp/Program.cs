9374a9c6a4384818db7fa6ab1238901c4ae4cd34
kubicek <82667968+kubicek-skoleni@users.noreply.github.com>
2025-02-17 13:43:08 +0100

    dny v tydnu, Zadávej číslo 1-7 nebo X pro ukončení.

--- CNET1/ConsoleApp/Program.cs
+++ CNET1/ConsoleApp/Program.cs
@@ -1,17 +1,36 @@
-﻿List<int> numbers = [2, 10, -555, 0, 12_000_000, -555];
+﻿//List<int> numbers = [2, 10, -555, 0, 12_000_000, -555];
+//List<int> numbers2 = new();
 
-List<int> numbers2 = new();
 
-numbers2.Add(50);
-var third = numbers.ElementAt(2);
+string[] dny = ["pondělí", "úterý", "středa", "čtvrtek",
+                "pátek", "sobota", "neděle"];
 
-Console.WriteLine($"treti element: {third}");
-
-foreach(int item in numbers)
+string DenVTydnu(int x)
 {
-    Console.WriteLine(item);
+    return dny[x - 1];
 }
 
+Console.WriteLine("Zadávej číslo 1-7 nebo X pro ukončení.");
+string input = Console.ReadLine();
 
+while(input != "X")
+{
+    int number = int.Parse(input);
+
+    if(number < 1 || number > 7)
+    {
+        Console.WriteLine("zadané číslo je mimo povolený rozsah");
+    }
+    else
+    {
+        string den_v_tydnu = DenVTydnu(number);
+        Console.WriteLine($"den v týdnu: {den_v_tydnu}");
+    }
+
+    Console.WriteLine("Zadávej číslo 1-7 nebo X pro ukončení.");
+    input = Console.ReadLine();
+}
+
+Console.WriteLine("ukončuji program");
 
 

59767fd771918125e53a360901823569fddc586f
kubicek <82667968+kubicek-skoleni@users.noreply.github.com>
2025-02-17 14:43:45 +0100

    ternarni operator uklid

--- CNET1/ConsoleApp/Den1.cs
+++ CNET1/ConsoleApp/Den1.cs
@@ -94,6 +94,21 @@ namespace ConsoleApp
             //    NEDELE
             //}
         }
+
+        void TernarniOperator()
+        {
+            int Bigger(int a, int b)
+            {
+
+                //ternarni operator
+                return (a > b) ? a : b;
+
+                //if (a > b)
+                //    return a;
+                //else
+                //    return b;
+            }
+        }
     }
     enum DenVTydnu
     {

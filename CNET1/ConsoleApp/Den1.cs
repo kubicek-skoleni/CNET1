839e383d3fe824fe21fd7c26c504e1aac7e4ae00
kubicek <82667968+kubicek-skoleni@users.noreply.github.com>
2025-02-18 08:56:00 +0100

    den1 uklid

--- CNET1/ConsoleApp/Den1.cs
+++ CNET1/ConsoleApp/Den1.cs
@@ -165,16 +165,35 @@ namespace ConsoleApp
                 return (age, hadBday);
             }
         }
-    }
-    enum DenVTydnu
-    {
-        PONDELI,
-        UTERY,
-        STREDA,
-        CTVRTEK,
-        PATEK,
-        SOBOTA,
-        NEDELE
-    }
-}
+
+
+        void den1konec()
+        {
+
+            string[] dny = ["pondělí", "úterý", "středa", "čtvrtek",
+                "pátek", "sobota", "neděle"];
+
+            (string den, bool vikend) DenVTydnuSlovo(DenVTydnu x)
+            {
+                bool vikend = (x == DenVTydnu.SOBOTA || x == DenVTydnu.NEDELE);
+                string den = dny[(int)x];
+                return (den, vikend);
+            }
+
+            var zadani = DenVTydnu.NEDELE;
+            var vysledek = DenVTydnuSlovo(zadani);
+            Console.WriteLine($"{zadani}: {vysledek.den}, vikend:{vysledek.vikend}");
+
+        }
+        enum DenVTydnu
+        {
+            PONDELI,
+            UTERY,
+            STREDA,
+            CTVRTEK,
+            PATEK,
+            SOBOTA,
+            NEDELE
+        }
+
 

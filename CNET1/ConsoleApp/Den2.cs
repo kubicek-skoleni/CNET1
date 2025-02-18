fe7c3523480df9b76622f664ac66870a851e8100
kubicek <82667968+kubicek-skoleni@users.noreply.github.com>
2025-02-18 09:29:51 +0100

    exceptioins uklid

---
+++ CNET1/ConsoleApp/Den2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Den2
+    {
+        void Exceptions()
+        {
+            string[] dny = ["pondělí", "úterý", "středa", "čtvrtek",
+                "pátek", "sobota", "neděle"];
+            string DenVTydnu(int x)
+            {
+                if (x < 0 || x > 6)
+                    throw new IndexOutOfRangeException();
+
+                return dny[x - 1];
+            }
+
+            try
+            {
+                var den = DenVTydnu(8);
+                Console.WriteLine($"den: {den}");
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine("index mimo rozsah pole");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("nastala chyba");
+            }
+            finally
+            {
+                Console.WriteLine("toto se provede vždy");
+            }
+        }
+    }
+}

66b47a37b89029c405d58b289330671b77dfdd4f
kubicek <82667968+kubicek-skoleni@users.noreply.github.com>
2025-02-19 09:47:09 +0100

    projiti ulou ze vcera

--- CNET1/ConsoleApp/Program.cs
+++ CNET1/ConsoleApp/Program.cs
@@ -11,39 +11,40 @@ Console.WriteLine("načteno");
 
 //// kolik jakych znacek aut dohromady (vsechny pujcovny)
 
-//CarBrand[] brands = (CarBrand[])Enum.GetValues(typeof(CarBrand));
+CarBrand[] brands = (CarBrand[])Enum.GetValues(typeof(CarBrand));
 
-//int sum = 0;
+int sum = 0;
 
-//foreach (var brand in brands)
-//{
-//    var cnt_1 = pujcovna1
-//                .CarsForRent.Where(car => car.Brand == brand)
-//                .Count();
-//    var cnt_2 = pujcovna2
-//                .CarsForRent.Where(car => car.Brand == brand)
-//                .Count();
+foreach (var brand in brands)
+{
+    var cnt_1 = pujcovna1
+                .CarsForRent.Where(car => car.Brand == brand)
+                .Count();
+    var cnt_2 = pujcovna2
+                .CarsForRent.Where(car => car.Brand == brand)
+                .Count();
 
-//    var cnt = cnt_1 + cnt_2;
-//    sum += cnt;
+    var cnt = cnt_1 + cnt_2;
+    sum += cnt;
 
-//    Console.WriteLine($"{brand}: {cnt}");
-//}
+    Console.WriteLine($"{brand}: {cnt}");
+}
 
-//Console.WriteLine(sum);
+Console.WriteLine(sum);
 
 /////////////////////////////////
 /////
 
-//var all_cars = pujcovna1.CarsForRent.Union(pujcovna2.CarsForRent);
+// sloučit obě kolekce aut
+var all_cars = pujcovna1.CarsForRent.Union(pujcovna2.CarsForRent);
 
-//var grouped = all_cars.GroupBy(x => x.Brand)
-//        .Select(x => new { Brand = x.Key, Count = x.Count() });
+var grouped = all_cars.GroupBy(car => car.Brand)
+        .Select(x => new { Brand = x.Key, Count = x.Count() });
 
-//foreach(var brand_count in grouped)
-//{
-//    Console.WriteLine($"brand:{brand_count.Brand}, count{brand_count.Count}");
-//}
+foreach (var brand_count in grouped)
+{
+    Console.WriteLine($"brand:{brand_count.Brand}, count{brand_count.Count}");
+}
 
 
 int a = 0;
@@ -57,7 +58,7 @@ Person p = null;
 DateTime? dt;
 DateOnly? dd = null;
 
-if(!string.IsNullOrEmpty(name))
+if (!string.IsNullOrEmpty(name))
 {
     name.ToLower();
 }
@@ -70,9 +71,9 @@ else
 string[] dny = ["pondělí", "úterý", "středa", "čtvrtek",
                 "pátek", "sobota", "neděle"];
 
-var result = dny.Select(word => new Car() { RegNumber = word});
+var result = dny.Select(word => new Car() { RegNumber = word });
 
-foreach(var num in result)
+foreach (var num in result)
 {
     Console.WriteLine(num);
 }

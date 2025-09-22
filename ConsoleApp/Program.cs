e450fe25b20e6590307e13459dddf28abe981a30
kubicek-skoleni <82667968+kubicek-skoleni@users.noreply.github.com>
2025-09-22 11:41:40 +0200

    den v tydnu pomoci if else

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,8 +1,32 @@
-﻿
-
-Console.WriteLine("Zadej číslo 1-7 a zmáčkni enter:");
+﻿Console.WriteLine("Zadej číslo 1-7 a zmáčkni enter:");
 
 string? line = Console.ReadLine();
+
+int dayNumber = int.Parse(line);
+
+if(dayNumber == 1)
+    Console.WriteLine("Pondělí");
+
+else if (dayNumber == 2)
+    Console.WriteLine("Úterý");
+
+else if (dayNumber == 3)
+    Console.WriteLine("Středa");
+
+else if (dayNumber == 4)
+    Console.WriteLine("Čtvrtek");
+
+else if (dayNumber == 5)
+    Console.WriteLine("Pátek");
+
+else if (dayNumber == 6)
+    Console.WriteLine("Sobota");
+
+else if (dayNumber == 7)
+    Console.WriteLine("Neděle");
+
+else
+    Console.WriteLine("Neplatné číslo dne v týdnu.");
 
 
 

adb57cdf9ed591cedea965ee60b4cbca6459d931
kubicek-skoleni <82667968+kubicek-skoleni@users.noreply.github.com>
2025-09-23 14:36:04 +0200

    student class

---
+++ ConsoleApp/Model/Student.cs
@@ -0,0 +1,24 @@
+﻿// třídu Student
+// jméno
+// datum narození
+// pohlaví
+// třida
+// info do konzole
+
+namespace ConsoleApp.Model
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public DateOnly BirthDate { get; set; }
+
+        public string Gender { get; set; }
+
+        public string ClassName { get; set; }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"{Name} ({BirthDate.ToString("dd. MM. yyyy")}) {ClassName}");
+        }
+    }
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -3,4 +3,15 @@
 // datum narození
 // pohlaví
 // třida
-// info do konzole
+// info do konzolep
+
+using ConsoleApp.Model;
+
+Student stud1 = new ();
+
+stud1.Name = "Jan Novák";
+stud1.BirthDate = new DateOnly(2012, 3, 15);
+stud1.ClassName = "7.A";
+stud1.Gender = "M";
+
+stud1.PrintInfo();
